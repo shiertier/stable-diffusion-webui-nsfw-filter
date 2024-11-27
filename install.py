aedbc6c25d983807d3cc4150a2503f8d52bf8027
shiertier <junjie.text@gmail.com>
2024-11-27 14:52:41 +0800

    fix install

--- install.py
+++ install.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import launch
 
 if not launch.is_installed("diffusers"):
     launch.run_pip(f"install diffusers", "diffusers")
 
 if not launch.is_installed("imgutils"):
-    launch.run_pip(f"install imgutils", "imgutils")
-=======
-import launch
-
-if not launch.is_installed("diffusers"):
-    launch.run_pip(f"install diffusers", "diffusers")
->>>>>>> eb139152b1f52fab3dedc59ab9fc68357bb502bd
+    launch.run_pip(f"install git+https://github.com/deepghs/imgutils.git@v0.7.0", "git+https://github.com/deepghs/imgutils.git@v0.7.0")
